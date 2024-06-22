0a8fe93b4f10832074d110bc984ddb30202b6555
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-06-22 08:35:32 -0500

    Proyecto creado

---
+++ src/main/java/com/cibertec/apireservaspanchita/ApiReservasPanchitaApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.apireservaspanchita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReservasPanchitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReservasPanchitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/apireservaspanchita/ApiReservasPanchitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.apireservaspanchita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReservasPanchitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
