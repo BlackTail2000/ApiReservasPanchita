501ca84e69828770dd5d07fc00b140e3b45fc3f8
carlosgabrielbacamanrique@hotmail.com <carlosgabrielbacamanrique@hotmail.com>
2024-06-27 18:29:47 -0500

    Reserva REST V2

--- src/main/java/com/cibertec/apireservaspanchita/service/IMesaService.java
+++ src/main/java/com/cibertec/apireservaspanchita/service/IMesaService.java
@@ -4,8 +4,13 @@ import com.cibertec.apireservaspanchita.model.dto.MesaDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 public interface IMesaService {
     MesaDto registrar(MesaDto mesaDto);
     MesaDto buscarPorId(Integer idMesa);
     MesaDto actualizarMesa(Integer idMesa, MesaDto mesaDto);
+    Integer obtenerNroMesasPorSucursal(Integer idSucursal);
+    Integer obtenerNroMesasOcupadasPorFechaYSucursal(Date fecha, Integer idSucursal);
+    Integer obtenerNroMesasDisponiblesPorFechaYSucursal(Date fecha, Integer idSucursal);
 }
